d62c46841974da840cc89a0df6aa35a3244af9d0
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-14 17:40:10 +0530

    Add project files.

---
+++ StudProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.WriteLine(student.display());
+            Student student1 = new Student();
+            Console.WriteLine(student1.display());
+        }
+    }
+}
---
+++ StudProject/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudProject
+{
+    public class Student
+    {
+        int rno, s1, s2, s3;
+        string sname;
+        float mark=150, perc;
+
+        public Student()
+        {
+            rno = 1;
+            sname = "Mahesh";
+        }
+        public void Student(int rno, string sname)
+        {
+            this.rno = rno;
+            this.sname = sname;
+        }
+        public int TotalMark()
+        {
+            mark = s1 + s2 + s3;
+        }
+        public float TotalPer()
+        {
+            per = mark / 3;
+        }
+        public string display()
+        {
+            return $"Student Detail:{rno}-{sname}-{mark}-{perc}";
+        }
+    }
+}    
+ 
